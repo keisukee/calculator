7cf0fae412a828e0579fff32a571f602c3c939d8
keisuke ohashi <kog71903@gmail.com>
2019-12-30 14:13:06 +0900

    [update]連続で計算記号が続かないように

--- main.js
+++ main.js
@@ -1,3 +1,29 @@
+let btn0Wrapper = document.getElementById('input-0');
+let btn0 = btn0Wrapper.querySelector('button');
+
+let outputBtn = document.getElementById('output-value')
+
+let undoWrapper = document.getElementById('undo');
+let undoBtn = undoWrapper.querySelector('button');
+
+let resetWrapper = document.getElementById('reset');
+let resetBtn = resetWrapper.querySelector('button');
+
+let plusWrapper = document.getElementById('plus');
+let plusBtn = plusWrapper.querySelector('button');
+
+let minusWrapper = document.getElementById('minus');
+let minusBtn = minusWrapper.querySelector('button');
+
+let multipleWrapper = document.getElementById('multiple');
+let multipleBtn = multipleWrapper.querySelector('button');
+
+let divideWrapper = document.getElementById('divide');
+let divideBtn = divideWrapper.querySelector('button');
+
+let equalWrapper = document.getElementById('equal');
+let equalBtn = equalWrapper.querySelector('button');
+
 function insertValue(inputBtn) {
   outputValue = outputBtn.innerText;
   if (outputValue === '0') {
@@ -11,12 +37,30 @@ function insertValue(inputBtn) {
 }
 
 function edit(elem) {
+  const length = outputBtn.innerText.length;
+  const formula = outputBtn.innerText;
+  const lastWord = outputBtn.innerText[length - 1];
+
   if (outputBtn.innerText === '0') {
     outputBtn.innerText = elem.innerText;
+  } else if (isSymbol(elem.innerText) && isSymbol(lastWord)) {
+    let replaceFormula = formula.substr(0, length - 1);
+    console.log('replaceFormula ', replaceFormula);
+    replaceFormula += elem.innerText;
+    outputBtn.innerText = replaceFormula;
   } else {
     outputBtn.innerText = outputBtn.innerText + elem.innerText;
   }
   console.log(outputBtn.innerText);
+}
+
+function isSymbol(symbol) {
+  console.log('symbol', symbol);
+  if (symbol === '+' || symbol === '-' || symbol === '*' || symbol === '/') {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 function calc() {
